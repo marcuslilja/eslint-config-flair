59518f29d90f0bee44a36b44972ad534f48e46df
Marcus Lilja <marcus.lilja@gmail.com>
2016-10-18 17:40:15 +0200

    Add initial linting rules for React

---
+++ react.js
@@ -0,0 +1,15 @@
+
+var rules = [
+  './rules/react/standard',
+  './rules/react/jsx'
+];
+
+module.exports = {
+  extends: rules.map(require.resolve),
+  plugins: ['react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
+};
